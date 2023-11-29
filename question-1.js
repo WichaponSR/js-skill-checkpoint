8a85e961cdb635b2f04fb0f681ce9ad752741585
WichaponSR <111661727+WichaponSR@users.noreply.github.com>
2023-11-29 13:18:24 +0700

    Update question-1.js

--- question-1.js
+++ question-1.js
@@ -6,3 +6,22 @@ const inventory = {
 };
 
 // เริ่มเขียนโค้ดตรงนี้
+
+inventory.apple.quantity = 300; 
+// apple: { price: 35, quantity: 300 }
+inventory.orange = {price: 20, quantity: 300}
+
+let sumAll = [];
+let result = 0;
+
+for(let count in inventory){
+  let sum = inventory[count].price * inventory[count].quantity
+    sumAll.push(sum);
+}
+
+for(let i=0; i<sumAll.length; i++){
+  result = result + sumAll[i];  
+}
+
+ console.log("Total inventory value: " + result + " baht");
+
