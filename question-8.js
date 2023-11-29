603722d53e9a2ba82f171ef6080e9563a53ef732
WichaponSR <111661727+WichaponSR@users.noreply.github.com>
2023-11-29 13:23:17 +0700

    Update question-8.js

--- question-8.js
+++ question-8.js
@@ -1,3 +1,13 @@
 // Question #8: Fetching User List from Server
 
 // เริ่มเขียนโค้ดตรงนี้
+
+fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response => response.json())
+  .then(data => {
+    const namesArray = data.map(user => user.name);
+    console.log(namesArray);
+  })
+  .catch(error => {
+    console.log('Error fetching data:', error);
+  });
